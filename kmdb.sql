8e67cb6bbca1b35207129236cd2abdf416ac6069
Anna Wickham <anna.wickham@kellogg.northwestern.edu>
2024-01-21 19:31:08 +0000

    homework draft 1

--- kmdb.sql
+++ kmdb.sql
@@ -104,10 +104,34 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
--- TODO!
+
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS actors;
+DROP TABLE IF EXISTS studios;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE movies (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    year_released INTEGER,
+    MPAA_rating TEXT,
+    studio_id INTEGER,
+    studio_name TEXT,
+)
+
+CREATE TABLE actors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    character_name TEXT,
+    studio_id INTEGER,
+    studio_name TEXT,
+)
+
+CREATE TABLE studios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+)
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
